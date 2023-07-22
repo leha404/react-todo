667ddad493d6ded08764492cc0b5be89a8efe202
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2023-07-22 17:54:16 +0500

    check list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,71 +1,70 @@
 function List() {
-  return <ul>
+  return <ol>
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={true}></input>
         <label> Компонент ToDo List</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Компонент Task</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Состояние tasks</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Добавление новой задачи</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Изменение статуса задача</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Удаление задачи</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Список задач на странице</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label> Фильтрация задач</label>
       </div>
     </li>
 
     <li>
       <div>
-        <input type="checkbox"></input>
+        <input type="checkbox" disabled checked={false}></input>
         <label>
-          {" "}
           Элементы управления для добавления, фильтрации и очистки списка задач
         </label>
       </div>
     </li>
-  </ul>;
+  </ol>;
 }
 
 export default List
