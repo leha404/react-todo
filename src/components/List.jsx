af4ade50f8b54f713c35642a13e89b46e9b13506
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2023-07-22 16:38:06 +0500

    first components

---
+++ src/components/List.jsx
@@ -0,0 +1,71 @@
+function List() {
+  return <ul>
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Компонент ToDo List</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Компонент Task</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Состояние tasks</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Добавление новой задачи</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Изменение статуса задача</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Удаление задачи</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Список задач на странице</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label> Фильтрация задач</label>
+      </div>
+    </li>
+
+    <li>
+      <div>
+        <input type="checkbox"></input>
+        <label>
+          {" "}
+          Элементы управления для добавления, фильтрации и очистки списка задач
+        </label>
+      </div>
+    </li>
+  </ul>;
+}
+
+export default List
--- src/index.js
+++ src/index.js
@@ -1,72 +1,7 @@
 // React dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Inline styling
-const customStyle = {
-  color: "red",
-}
-
-// Update Custom Style JSON
-customStyle.color = "blue"
-
-// App Component
-const App = () => {
-  // Injecting code into html (JSX) is like {var}
-  const name = 'Username :)'
-
-  return (
-    <div>
-      <h1 className='heading'>Hello, {name}!</h1>
-
-      <p style={customStyle}>План по ReadMe:</p>
-      <div>
-        <input type='checkbox'></input>
-        <label> Компонент ToDo List</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Компонент Task</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Состояние tasks</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Добавление новой задачи</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Изменение статуса задача</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Удаление задачи</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Список задач на странице</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Фильтрация задач</label>
-      </div>
-
-      <div>
-        <input type='checkbox'></input>
-        <label> Элементы управления для добавления, фильтрации и очистки списка задач</label>
-      </div>
-    </div>
-  );
-};
+import App from "./App"
 
 // What and Where to render
 ReactDOM.render(<App />, document.getElementById('root'));
