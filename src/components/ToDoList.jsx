805b3e5a013bb9ddf988a2099ddd4192289e2497
Aleksey Chernavskiy <alekseychernavskiy@gmail.com>
2023-08-01 18:09:40 +0500

    mapping components from source

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,42 +1,15 @@
 import Task from "./Task";
+import tasks from "../tasks";
+
+function createTask(task) {
+  return <li>
+    <Task key={task.name} checked={task.checked} text={task.text}/>
+  </li>;
+}
 
 function ToDoList() {
   return <ol>
-    <li>
-      <Task checked={true} text="Компонент ToDo List - 2023-07-22"/>
-    </li>
-
-    <li>
-      <Task checked={true} text="Компонент Task - 2023-07-23"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Состояние tasks"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Добавление новой задачи"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Изменение статуса задача"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Удаление задачи"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Список задач на странице"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Фильтрация задач"/>
-    </li>
-
-    <li>
-      <Task checked={false} text="Элементы управления для добавления, фильтрации и очистки списка задач"/>
-    </li>
+    {tasks.map(createTask)}
   </ol>;
 }
 
---
+++ src/tasks.js
@@ -0,0 +1,40 @@
+const tasks = [
+    {
+        checked: true,
+        text: "Компонент ToDo List - 2023-07-22"
+    },
+    {
+        checked: true,
+        text: "Компонент Task - 2023-07-23"
+    },
+    {
+        checked: false,
+        text: "Состояние tasks"
+    },
+    {
+        checked: false,
+        text: "Добавление новой задачи"
+    },
+    {
+        checked: false,
+        text: "Изменение статуса задача"
+    },
+    {
+        checked: false,
+        text: "Удаление задачи"
+    },
+    {
+        checked: false,
+        text: "Список задач на странице"
+    },
+    {
+        checked: false,
+        text: "Фильтрация задач"
+    },
+    {
+        checked: false,
+        text: "Элементы управления для добавления, фильтрации и очистки списка задач"
+    }
+]
+
+export default tasks;
